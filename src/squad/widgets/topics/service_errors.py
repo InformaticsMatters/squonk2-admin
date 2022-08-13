a36051de9dbb0e9c2b52758dde6bd50a6120984a
Alan Christie <alan.christie@matildapeak.com>
2022-08-13 12:15:12 +0100

    fix: Adds sort to service errors (untested)

--- src/squad/widgets/topics/service_errors.py
+++ src/squad/widgets/topics/service_errors.py
@@ -1,19 +1,35 @@
 """A textual widget used to display DM Service Errors.
 """
 from datetime import datetime
+from typing import Any, Dict, List, Tuple
 
+import pandas
 from rich.panel import Panel
-from rich.table import Table
+from rich.style import Style
 from rich.text import Text
 from squonk2.dm_api import DmApi
 
 from squad import common
 from squad.access_token import AccessToken
-from .base import TopicRenderer
+from .base import SortOrder, TopicRenderer
+
+# List of columns using names, styles and justification.
+# some styles are dynamic.
+_COLUMNS: List[Tuple[str, Style, str]] = [
+    ("ID", common.ITEM_KEY_STYLE, "right"),
+    ("Time (UTC)", common.DATE_STYLE, "left"),
+    ("Severity", None, "left"),
+    ("Summary", common.SIZE_STYLE, "left"),
+]
 
 
 class ServiceErrors(TopicRenderer):
     """Displays service errors."""
+
+    def __init__(self) -> None:
+        # Default sort column
+        self.num_columns = len(_COLUMNS)
+        self.sort_column = 1
 
     def render(self) -> Panel:
         """Render the widget."""
@@ -36,32 +52,44 @@ class ServiceErrors(TopicRenderer):
             else:
                 self.last_response = None
 
-        # Results are presented in a table.
-        table: Table = Table(
-            collapse_padding=True,
-            header_style=common.INDEX_STYLE,
-            box=None,
-        )
-        table.add_column("", style=common.INDEX_STYLE, no_wrap=True, justify="right")
-        table.add_column("ID", style=common.ITEM_KEY_STYLE, no_wrap=True)
-        table.add_column("Time (UTC)", style=common.ITEM_KEY_STYLE, no_wrap=True)
-        table.add_column("Severity", style=common.ITEM_KEY_STYLE, no_wrap=True)
-        table.add_column("Summary", style=common.SIZE_STYLE, no_wrap=True)
+        # Results in a table.
+        self.prepare_table(_COLUMNS)
+        assert self.table
 
         # Populate rows based on the last response.
+        # We populate 'data' with the project material
+        # so that we can sort on 'launched' date using pandas.
+        data: Dict[str, List[Any]] = {}
+        row_number: int = 1
         if self.last_response and self.last_response.success:
             for error in self.last_response.msg["service_errors"]:
-                table.add_row(
-                    str(table.row_count + 1),
-                    str(error["id"]),
+                data[f"{row_number}"] = [
+                    error["id"],
                     error["created"],
                     error["severity"],
                     error["summary"],
+                ]
+                row_number += 1
+
+        # Populate rows based on the last response.
+        if data:
+            data_frame: pandas.DataFrame = pandas.DataFrame.from_dict(
+                data, orient="index"
+            )
+            for _, row in data_frame.sort_values(
+                by=[self.sort_column], ascending=self.sort_order == SortOrder.ASCENDING
+            ).iterrows():
+                self.table.add_row(
+                    str(self.table.row_count + 1),
+                    str(row[0]),
+                    row[1],
+                    row[2],
+                    row[3],
                 )
 
-        title: str = f"Service errors ({table.row_count})"
+        title: str = f"Service errors ({self.table.row_count})"
         return Panel(
-            table if table.row_count else Text(),
+            self.table if self.table.row_count else Text(),
             title=title,
             style=common.CORE_STYLE,
             padding=0,
